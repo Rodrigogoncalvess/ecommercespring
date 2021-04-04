ca272b98c7b44ebd619922143d8b04da3624b9cf
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-04-04 13:26:38 -0300

    primeiro commit

---
+++ src/main/java/com/gama/ecommercespring/EcommercespringApplication.java
@@ -0,0 +1,14 @@
+package com.gama.ecommercespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercespringApplication.class, args);
+	}
+
+
+}
